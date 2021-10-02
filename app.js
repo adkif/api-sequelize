8efa60423764c0ff864846010e846976e611504d
adkif <akifungo@gmail.com>
2021-10-02 08:58:35 +0200

    index update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require('./config/db.config');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
