1ec5b6bb3f2297bb7f8dfc4360a897371a423f09
adkif <akifungo@gmail.com>
2021-10-03 21:08:03 +0200

    add relation between order and cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,40 +1,47 @@
-const cart = require('../models/cart.model');
-
+const cart = require("../models/cart.model");
+const order = require("../models/order.model");
+cart.hasMany(order, { foreignKey: "id" });
+order.belongsTo(cart, { foreignKey: "cartId" });
 // Create and Save a new cart
 exports.create = (req, res) => {
   // Validate request
   if (!req.body) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content can not be empty!",
     });
     return;
   }
   // Save cart in the database
-  cart.create(req.body)
-    .then(data => {
+  cart
+    .create(req.body, { include: order })
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the cart."
+        message: err.message || "Some error occurred while creating the cart.",
       });
     });
 };
 
 // Retrieve all carts from the database.
 exports.findAll = (req, res) => {
-  cart.findAll({
-    where: {
-      status: 1,
-    },
-  })
-    .then(carts => {
+  cart
+    .findAll({
+      where: {
+        status: 1,
+      },
+      include: [{
+        model: order,
+        require: true
+      }]
+    })
+    .then((carts) => {
       res.send(carts);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error retrieving carts =" + err
+        message: "Error retrieving carts =" + err,
       });
     });
 };
@@ -42,18 +49,23 @@ exports.findAll = (req, res) => {
 // Find a single cart with an id
 exports.findOne = (req, res) => {
   const id = req.params.cartId;
-  cart.findOne({
-    where: {
-      id: id,
-      status: 1,
-    },
-  })
-    .then(data => {
+  cart
+    .findOne({
+      where: {
+        id: id,
+        status: 1,
+      },
+      include: [{
+        model: order,
+        require: true
+      }]
+    })
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error retrieving cart with id=" + id
+        message: "Error retrieving cart with id=" + id,
       });
     });
 };
@@ -62,23 +74,28 @@ exports.findOne = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.cartId;
 
-  cart.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
+  cart
+    .update(req.body, {
+      where: { id: id },
+      include: [{
+        model: order,
+        require: true
+      }]
+    })
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "cart was updated successfully."
+          message: "cart was updated successfully.",
         });
       } else {
         res.send({
-          message: `Cannot update cart with id=${id}. Maybe cart was not found or req.body is empty!`
+          message: `Cannot update cart with id=${id}. Maybe cart was not found or req.body is empty!`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error updating cart with id=" + id
+        message: "Error updating cart with id=" + id,
       });
     });
 };
@@ -87,40 +104,41 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   const id = req.params.cartId;
 
-  cart.destroy({
-    where: { id: id }
-  })
-    .then(num => {
+  cart
+    .destroy({
+      where: { id: id },
+    })
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "cart was deleted successfully!"
+          message: "cart was deleted successfully!",
         });
       } else {
         res.send({
-          message: `Cannot delete cart with id=${id}. Maybe cart was not found!`
+          message: `Cannot delete cart with id=${id}. Maybe cart was not found!`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Could not delete cart with id=" + id
+        message: "Could not delete cart with id=" + id,
       });
     });
 };
 
 // Delete all carts from the database.
 exports.deleteAll = (req, res) => {
-  cart.destroy({
-    where: {},
-    truncate: false
-  })
-    .then(nums => {
+  cart
+    .destroy({
+      where: {},
+      truncate: false,
+    })
+    .then((nums) => {
       res.send({ message: `${nums} carts were deleted successfully!` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all carts."
+        message: err.message || "Some error occurred while removing all carts.",
       });
     });
 };
